49b0a8c932a64568717853d08a2ef9ea484240e0
Vamshi Krishna Janagani <vamshijanagani224@gmail.com>
2024-10-10 22:41:50 +0530

    Added Linear Search

--- Advanced problems/Arrays/Fundamentals/Linear Search.cpp
+++ Advanced problems/Arrays/Fundamentals/Linear Search.cpp
@@ -1,1 +1,29 @@
 //Linear Search problem
+//Input: nums = [2, 3, 4, 5, 3], target = 3
+//Output: 1
+//Explanation: The first occurence of 3 in nums is at index 1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int linearSearch(vector<int>& nums, int target){
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+
+int main(){
+    Solution sol;
+    vector<int>nums={2,3,4,5,3};
+    int target=3;
+    int ans=sol.linearSearch(nums,target);
+    cout<<"Target found at index "<<ans;
+    return 0;
+}
