d07c92769632e81d590f9e4e49e0b2a050ed3118
Vamshi Krishna Janagani <vamshijanagani224@gmail.com>
2024-10-13 16:40:29 +0530

    Edited with optimal approach Move Zeros to the end

--- Advanced problems/Arrays/Logic Building/Move Zeros to End.cpp
+++ Advanced problems/Arrays/Logic Building/Move Zeros to End.cpp
@@ -8,19 +8,38 @@ using namespace std;
 class Solution{
     public:
     void moveZeroes(vector<int>& nums){
-        //Brute Force approach
-        vector<int>temp; //Creating temp array
+        //Optimal approach , two pointer 
+        int j=-1;
+        //Finding the first 0th index
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j=i;
+                break;
             }
-        }//stored non zero elememts in temp array
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp[i];
-        }//Restoring back non zero elements in original array
-        for(int i=temp.size();i<nums.size();i++){
-            nums[i]=0;
-        }//storing zeros at the end of the array
+        }
+        if (j == -1) return;
+        //Now comparing both i and j elements
+        for(int i=j+1;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+
+        
+        // //Brute Force approach
+        // vector<int>temp; //Creating temp array
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //         temp.push_back(nums[i]);
+        //     }
+        // }//stored non zero elememts in temp array
+        // for(int i=0;i<temp.size();i++){
+        //     nums[i]=temp[i];
+        // }//Restoring back non zero elements in original array
+        // for(int i=temp.size();i<nums.size();i++){
+        //     nums[i]=0;
+        // }//storing zeros at the end of the array
     }
 };
 
