63cf522ea25caee1c26208bff9c1228caf4d29de
Vamshi Krishna Janagani <vamshijanagani224@gmail.com>
2024-12-02 18:10:14 +0530

    Added Kadans algorithm

---
+++ Advanced problems/Arrays/FAQS Medium/Kadanes Algorithm.cpp
@@ -0,0 +1,33 @@
+//Input: nums = [2, 3, 5, -2, 7, -4]
+//Output: 15
+//Explanation: The subarray from index 0 to index 4 has the largest sum = 15
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int maxSubArray(vector<int>& nums) {
+        int larg=INT_MIN;
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum<0){
+                sum=0;
+            }
+            if(sum>larg){
+                larg=sum;
+            }
+        }
+        return larg;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int>nums={2,3,5,-2,7,-4};
+    int ans=sol.maxSubArray(nums);
+    cout<<"Sum of maximum sub array is "<<ans;
+    return 0;
+}
