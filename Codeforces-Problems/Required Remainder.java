93c83f4a00062e11e34824c4c9740205b457cf18
Vamshi Krishna Janagani <87365180+VamshiJanagani@users.noreply.github.com>
2026-05-17 20:12:47 +0530

    POTD

---
+++ Codeforces-Problems/Required Remainder.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public final class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int n = sc.nextInt();
+            int ans = n - (n-y)%x;
+            System.out.println(ans);
+        }
+	}
+}
